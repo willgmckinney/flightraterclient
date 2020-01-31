5e7e38be2660241ee70138560f832b20e42ec3cf
William Mckinney <williammckinney@Williams-MacBook-Pro.local>
2020-01-31 14:29:39 -0500

    Update

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -3,8 +3,8 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:8000";
     break;
-  case "wm-flightrater-server.herokuapp.com":
-    APIURL = "https://wm-flightrater-client.herokuapp.com"
+  case "wm-flightrater-client.herokuapp.com/":
+    APIURL = "https://wm-flightrater-server.herokuapp.com"
     break;
 }
 export default APIURL;
